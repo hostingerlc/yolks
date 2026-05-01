ca6d2503c819a340f823a52aa8588a9da72c3fb3
Lezetho <lezethoyt@gmail.com>
2026-05-01 16:59:44 -0400

    TRY to do swift yolk lmao

---
+++ swift/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print Node.js Version
+node -v
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo ""
+echo "Running on .host - discord.gg/dothost"
+echo ""
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
